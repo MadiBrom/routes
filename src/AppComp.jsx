c2dde3c902f4885340a0aa22b0d334f17943bfad
Madi Bromfield <madibromfield215@gmail.com>
2024-08-25 23:49:40 -0500

    made it work

--- src/AppComp.jsx
+++ src/AppComp.jsx
@@ -1,32 +1,28 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Blue from "./components/Blue";
+import Red from "./components/Red";
 import "./App.css";
-// import Blue from "./components/blue.jsx";
-// import Red from "./components/red.jsx";
-// import Home from "./components/home.jsx";
-import Test from "./components/test";
 
 function App() {
   return (
     <Router>
       <div id="container">
         <div id="navbar">
-          {/* <Link to="/">Home</Link>
+          <Link to="/">Home</Link>
           <Link to="/blue">Blue</Link>
-          <Link to="/red">Red</Link> */}
-          <Link to="/test">Test</Link>
+          <Link to="/red">Red</Link>
         </div>
         <div id="main-section">
           <Routes>
-            {/* <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/blue" element={<Blue />} />
-            <Route path="/red" element={<Red />} /> */}
+            <Route path="/red" element={<Red />} />
             <Route path="*" element={<h1>404 Not Found</h1>} />
-            <Route path="/test" element={<Test />} />
           </Routes>
         </div>
       </div>
     </Router>
   );
 }
-console.log("App component loaded");
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import App from "./AppComp.jsx";
+import App from "./AppComp.jsx";
 import "./index.css";
-import Test from "./components/test";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Test />
+    <App />
   </StrictMode>
 );
